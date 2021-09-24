4a4f5f50759125bf4d97b3b25fda8c881b00766d
Cedric <l3c43v1039p@gmail.com>
2021-09-24 17:40:09 +0800

    Update index.js

--- store/index.js
+++ store/index.js
@@ -22,51 +22,7 @@ export const getters ={
 
 
 export const mutations = {
-    addItem(state, params = Object){
-        if(state.user.length != 0){
-            if(state.cart.length != 0 && params[5] <= state.pointbalance){
-
-                
-                var Exist = state.cart.findIndex( s => s.id == params[0])
-
-                if(Exist < 0){
-                    state.cart.push({
-                        id: params[0],
-                        name: params[1],
-                        image: params[2],
-                        class: params[3],
-                        stats:params[4]
-                    })
-
-                    state.pointbalance = state.pointbalance - params[5]
-                    const currentUser =  Moralis.User.current();
-                    currentUser.set("cart",state.cart)
-                    currentUser.set("balance",state.pointbalance)
-                    currentUser.save()
-                }
-
-            }else{
-                if(params[5] <= state.pointbalance){
-                    
-                    state.cart.push({
-                        id: params[0],
-                        name: params[1],
-                        image: params[2],
-                        class: params[3],
-                        stats:params[4]
-                    })
-
-                    state.pointbalance = state.pointbalance - params[5]
-                    const currentUser =  Moralis.User.current();
-                    currentUser.set("cart",state.cart)
-                    currentUser.set("balance",state.pointbalance)
-                    currentUser.save()
-                    
-                }
-                
-            }
-        }
-    },
+   
 
     authorize_loggin(state,user){
         return state.user = user;
